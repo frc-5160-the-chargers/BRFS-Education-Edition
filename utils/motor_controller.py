28d9c906a246ba7d689e8f3a669a019177ea0425
battery-staple <batterystapledev@gmail.com>
2020-12-04 15:24:16 -0500

    Made MotorController inherit from SpeedController.

--- utils/motor_controller.py
+++ utils/motor_controller.py
@@ -1,20 +1,42 @@
 import rev
+import wpilib.interfaces
+from wpilib.interfaces._interfaces import SpeedController
 
 
-class MotorController:
+class MotorController(SpeedController):
 
     def __init__(self, motor, maximum_safe_speed: float = 0.01):
+        super().__init__()
         self.motor = motor
         self.maximum_safe_speed = maximum_safe_speed
 
-    def set(self, power: float):
+    @staticmethod
+    def validate_input(self, power: float) -> float:
         if power > 0.01:
             raise ValueError("WHAT ARE YOU DOING ARE YOU TRYING TO BREAK THE ROBOT")
         else:
-            self.motor.set(power)
+            return power
 
     def stop(self):
         self.motor.stop_motor()
+
+    def get(self) -> float:
+        return self.motor.get()
+
+    def set(self, power: float):
+        self.motor.set(self.validate_input(power))
+
+    def getInverted(self) -> bool:
+        return self.motor.getInverted()
+
+    def setInverted(self, isInverted: bool) -> None:
+        self.motor.setInverted(isInverted)
+
+    def setVoltage(self, output) -> None:
+        self.motor.setVoltage(output)
+
+    def stopMotor(self) -> None:
+        self.motor.stopMotor()
 
 
 class SparkMaxMotorController(MotorController):
